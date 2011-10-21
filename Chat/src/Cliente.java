215a82a0d48f21d9ff4c0668552fc7e54742b9c8
wilramone@hotmail.com <wilramone@hotmail.com@8f8e76c6-8196-ec36-0685-80ad8fed11d2>
2011-10-21 14:57:55 +0000

    Criação da Estrutura

---
+++ Chat/src/Cliente.java
@@ -0,0 +1,89 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ConnectException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import javax.swing.JOptionPane;
+
+public class Cliente {
+
+	public static Janela cli;
+
+	public static boolean enviar = false;
+
+	public static void main(String[] args) throws UnknownHostException,
+			IOException {
+
+		Janela cli = new Janela();
+		cli.criaJanela();
+
+		cli.btn_enviar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				click(e);
+			}
+		});
+
+		new Cliente("10.72.2.138", 8080, cli).executa();
+
+	}
+
+	private String host;
+	private int porta;
+	public String nome;
+
+	public Cliente(String host, int porta, Janela cli) {
+		this.host = host;
+		this.porta = porta;
+		this.nome = cli.msg;
+		this.cli = cli;
+	}
+
+	public void executa() throws UnknownHostException, IOException {
+		Socket cliente = null;
+		try {
+			cliente = new Socket(this.host, this.porta);
+			System.out.println("O cliente se conectou ao servidor!");
+		} catch (ConnectException e) {
+			JOptionPane.showMessageDialog(null,
+					"Erro ao conectar com o Servidor", "Erro",
+					JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
+		cliente = new Socket(this.host, this.porta);
+		System.out.println("O cliente se conectou ao servidor!");
+
+		// thread para receber mensagens do servidor
+		Recebedor r = new Recebedor(cliente.getInputStream(), this);
+		new Thread(r).start();
+		PrintStream saida = null;
+
+		do {
+			while (!enviar) {
+			}
+			cliente = new Socket(this.host, this.porta);
+			saida = new PrintStream(cliente.getOutputStream());
+			saida.println(nome + ": " + cli.enviar.getText() + "\r\n");
+			saida.close();
+			cliente.close();
+			cli.enviar.setText("");
+			enviar = false;
+		} while (true);
+
+		// le msgs do teclado e manda pro servidor
+		// Scanner teclado = new Scanner(System.in);
+		// PrintStream saida = new PrintStream(cliente.getOutputStream());
+		// while (teclado.hasNextLine()) {
+		// saida.println(nome+": "+teclado.nextLine()+"\r\n");
+		// }
+		// teclado.close();
+
+	}
+
+	public static void click(ActionEvent e) {
+		enviar = !cli.enviar.getText().isEmpty();
+	}
+
+}
---
+++ Server/src/servico/Servidor.java
@@ -0,0 +1,50 @@
+package servico;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Servidor {
+
+ public static void main(String[] args) throws IOException {
+ // inicia o servidor
+ new Servidor(8080).executa();
+ }
+ private int porta;
+ private boolean encerrar;
+ private List<PrintStream> clientes;
+ public Servidor (int porta) {
+ this.porta = porta;
+ this.clientes = new ArrayList<PrintStream>();
+ }
+
+ public void executa () throws IOException {
+ ServerSocket servidor = new ServerSocket(this.porta);
+ System.out.println("Porta 8080 aberta!");
+
+ while (!encerrar) {
+ // aceita um cliente
+ Socket cliente = servidor.accept();
+ System.out.println("Nova conexão com o cliente "
+		            +cliente.getInetAddress().getHostAddress());
+
+ // adiciona saida do cliente à lista
+ PrintStream ps = new PrintStream(cliente.getOutputStream());
+ this.clientes.add(ps);
+
+ // cria tratador de cliente numa nova thread
+ TrataCliente tc = new TrataCliente(cliente.getInputStream(), this);
+new Thread(tc).start();
+ }
+
+ }
+
+ public void distribuiMensagem(String msg) {
+ // envia msg para todo mundo
+ for (PrintStream cliente : this.clientes) {
+ cliente.println(msg);
+ }
+ }
+}
