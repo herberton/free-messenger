0fcfe981856b389f98e5ef47ca678852853d40fa
wilramone@hotmail.com <wilramone@hotmail.com@8f8e76c6-8196-ec36-0685-80ad8fed11d2>
2011-10-21 14:57:55 +0000

    Criação da Estrutura

---
+++ trunk/Chat/src/Recebedor.java
@@ -0,0 +1,26 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Recebedor implements Runnable {
+
+ private InputStream servidor;
+ private Cliente cliente ;
+
+ public Recebedor(InputStream servidor,Cliente cliente) {
+ this.servidor = servidor;
+ this.cliente=cliente;
+ 
+ }
+
+ public void run() {
+ // recebe msgs do servidor e imprime na tela
+ Scanner s = new Scanner(this.servidor);
+ while (s.hasNextLine()) {
+ String mensagem=s.nextLine();
+ if(mensagem.length()>1){
+ System.out.println(mensagem);
+ cliente.cli.conversa.append(mensagem+"\r\n");	 
+ }
+ }
+ }
+}
